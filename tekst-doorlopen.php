aaf41478e5d0fe10a6c6b08f6c1244d7d35cfd18
SWCL0 <stan.w.c@icloud.com>
2024-02-29 09:27:27 +0100

    update tekst-doorlopen.php
    toevoeging telling:
    - characters
    - woorden
    - spaties
    - klinkers

--- tekst-doorlopen.php
+++ tekst-doorlopen.php
@@ -3,6 +3,31 @@
 echo "Geef mij een tekst!" . PHP_EOL;
 $tekstInput = readline();
 
+echo strlen($tekstInput) . " characters in de tekst." . PHP_EOL;
+echo str_word_count($tekstInput, 0) . " woorden in de tekst." . PHP_EOL;
+echo substr_count($tekstInput, ' ') . " spaties in de tekst." . PHP_EOL;
+$str = $tekstInput;
+$klinkerCount = countKlinkers($str);
+echo $klinkerCount . "klinkers in de tekst." . PHP_EOL;
 
+
+
+
+
+
+
+
+function countKlinkers($str) 
+{
+    $str = strtolower($str);
+    $klinkerCount = 0;
+
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], ["a", "e", "i", "o", "u"])) {
+            $klinkerCount++;
+        }
+    }
+    return $klinkerCount;
+}
 
 ?>
