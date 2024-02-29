d1d6b9d7810b55b072ab9133d7d04e1dc909f6b5
SWCL0 <stan.w.c@icloud.com>
2024-02-29 09:37:46 +0100

    update tekst-doorlopen.php
    toevoeging:
    - medeklinker
    - comma
    - punt
    - dubbelepunt
    - aanhalingsteken

--- tekst-doorlopen.php
+++ tekst-doorlopen.php
@@ -9,13 +9,26 @@ echo substr_count($tekstInput, ' ') . " spaties in de tekst." . PHP_EOL;
 $str = $tekstInput;
 $klinkerCount = countKlinkers($str);
 echo $klinkerCount . "klinkers in de tekst." . PHP_EOL;
+$medeKlinkerCount = countMedeKlinkers($str);
+echo $medeKlinkerCount . "medeklinkers in de tekst." . PHP_EOL;
+echo substr_count($tekstInput, ',') . " comma's in de tekst." . PHP_EOL;
+echo substr_count($tekstInput, '.') . " punten in de tekst." . PHP_EOL;
+echo substr_count($tekstInput, ':') . " dubbelepunten in de tekst." . PHP_EOL;
+echo substr_count($tekstInput, '"') . " aanhalingstekens in de tekst." . PHP_EOL;
 
 
+function countMedeKlinkers($str) 
+{
+    $str = strtolower($str);
+    $medeKlinkerCount = 0;
 
-
-
-
-
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"])) {
+            $medeKlinkerCount++;
+        }
+    }
+    return $medeKlinkerCount;
+}
 
 function countKlinkers($str) 
 {
